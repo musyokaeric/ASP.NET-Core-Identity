fc22ad3960013ed5100bb2bc38c4e7b8e8323615
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-11-15 12:28:27 +0300

    Implemented Verification and Generated Cookie with Cookie Authentication Handler
    To view the cookie, open the browser development tools (inspect). Go to Application, Cookies and select you domain url.

--- UnderTheHood/Pages/Account/Login.cshtml.cs
+++ UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace UnderTheHood.Pages.Account
 {
@@ -13,9 +15,31 @@ namespace UnderTheHood.Pages.Account
         {
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
+            if (!ModelState.IsValid) return Page();
 
+            // Verification is successful
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                // Create the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                // Serialize the ClaimsPrinciple into a string, encrypt the string, and
+                // save it as a cookie in the HttpContext object
+                await HttpContext.SignInAsync("MyCookieAuth", principal);
+
+                return RedirectToPage("/Index");
+            }
+
+            // Verification failed
+            return Page();
         }
     }
 
