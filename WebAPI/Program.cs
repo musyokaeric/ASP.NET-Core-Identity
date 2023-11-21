2d7b68337097e36517a5ab8ba72742a13ca90b08
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-11-21 21:01:32 +0300

    Applied Policy Based Authorization to Web API Endpoint

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -31,6 +31,12 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+// Configure Policy Based Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireClaim("Admin"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
