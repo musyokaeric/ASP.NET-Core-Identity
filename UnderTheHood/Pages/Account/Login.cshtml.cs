44092d26c0c30b573d2283cc9f6df57eae939d33
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-11-21 19:58:35 +0300

    Cleanup Code

--- UnderTheHood/Pages/Account/Login.cshtml.cs
+++ UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
+using UnderTheHood.Authorization;
 
 namespace UnderTheHood.Pages.Account
 {
@@ -55,19 +56,5 @@ namespace UnderTheHood.Pages.Account
             // Verification failed
             return Page();
         }
-    }
-
-    public class Credential
-    {
-        [Required]
-        [Display(Name = "User Name")]
-        public string UserName { get; set; } = string.Empty;
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; } = string.Empty;
-
-        [Display(Name ="Remember Me")]
-        public bool RememberMe { get; set; }
     }
 }
