0c2055d97beb51af684dcdb5bd54cb0a18aca977
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-11-22 14:03:29 +0300

    Create the Database for Identity

--- WebDotNetIndentity/Program.cs
+++ WebDotNetIndentity/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebDotNetIndentity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Injected DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
